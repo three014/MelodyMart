06e668e52c600b258894b7c1c980909330ebf378
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-07-14 18:46:58 -0500

    Add files via upload
    Added model discount and route discount, updated product to include quantity. I thought we could skip over quantity.

--- ecommerceapi/models/Product.js
+++ ecommerceapi/models/Product.js
@@ -10,6 +10,7 @@ const ProductSchema = new mongoose.Schema(
         color: { type: Array },
         price: { type: Number, required:true },
         inStock: { type: Boolean, default: true },
+        quantity: {type: Number, default: 0, required: true }
     },
     { timestamps: true }
 );
---
+++ ecommerceapi/routes/discount.js
@@ -0,0 +1,57 @@
+const Discount = require("../models/Discount");
+const {
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+const router = require("express").Router();
+
+//CREATE
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newDiscount = new Discount(req.body);
+
+  try {
+    const savedDiscount = await newDiscount.save();
+    res.status(200).json(savedDiscount);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedDiscount = await Discount.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedDiscount);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Discount.findByIdAndDelete(req.params.id);
+    res.status(200).json("Discount has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET PRODUCT
+router.get("/find/:id", async (req, res) => {
+  try {
+    const discount = await Discount.findById(req.params.id);
+    res.status(200).json(discount);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
