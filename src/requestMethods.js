2db2ae45c933970c8d7c467b36eaaf5bee86fbdb
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-07-19 14:42:51 -0500

    Update requestMethods.js
    Updated TOKEN to match current admin2 token. Be aware, if relying on admin token to update and check periodically to ensure that the token being used is the most up to date.

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 //const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWI4ZjkzMmI5MDU4ODg0NjRlMmZkYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4ODk2NTA0NSwiZXhwIjoxNjg5MjI0MjQ1fQ.TwbhQXUpWO9JZ6uP-hiEvIg04w9zopTYiwiHnqszSvY";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWI4ZjkzMmI5MDU4ODg0NjRlMmZkYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4OTc5NDY1NCwiZXhwIjoxNjkwMDUzODU0fQ.iUhzHVuW4DgNq2mBWgAbo0zCwBtXaAmta8QUbonaEjA";
 /*
 const TOKEN = () => {
     if (
