f251ea1f06ef280b647f070e45a3e0e98b91b3c8
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-07-20 14:47:29 -0500

    Update requestMethods.js
    One of the reasons token was not valid was due to error in userRequest, header should be headers. Missing an 's'.

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-//const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWI4ZjkzMmI5MDU4ODg0NjRlMmZkYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4OTc5NDY1NCwiZXhwIjoxNjkwMDUzODU0fQ.iUhzHVuW4DgNq2mBWgAbo0zCwBtXaAmta8QUbonaEjA";
+
+// Token parse works
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+
+//const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWI4ZjkzMmI5MDU4ODg0NjRlMmZkYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4OTc5NDY1NCwiZXhwIjoxNjkwMDUzODU0fQ.iUhzHVuW4DgNq2mBWgAbo0zCwBtXaAmta8QUbonaEjA";
 /*
 const TOKEN = () => {
     if (
@@ -14,11 +17,12 @@ const TOKEN = () => {
     } else { return "" }
 };
 */
+
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    header:{token:`Bearer ${TOKEN}` },
+    headers:{token:`Bearer ${TOKEN}` },
 });
