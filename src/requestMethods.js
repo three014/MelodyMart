97f32c7f49a350bbace7fb3bc16cd462964e8516
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-07-20 14:49:39 -0500

    Update requestMethods.js
    Added spaces in the userRequest to make it prettier.

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -24,5 +24,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    headers:{token:`Bearer ${TOKEN}` },
+    headers: { token: `Bearer ${TOKEN}` },
 });
