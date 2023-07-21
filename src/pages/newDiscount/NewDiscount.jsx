d3070132cd23867120e6812cce79f3f9043deae3
Lasya Yakkala <lasya.yakkala@gmail.com>
2023-07-21 14:29:26 -0500

    Fixed create new discount functionality

--- src/pages/newDiscount/NewDiscount.jsx
+++ src/pages/newDiscount/NewDiscount.jsx
@@ -1,77 +1,24 @@
 import "./newDiscount.css"
 import { useState } from "react";
-import {
-    getStorage,
-    ref,
-    uploadBytesResumable,
-    getDownloadURL,
-  } from "firebase/storage";
-  import app from "../../firebase";
-  import { addDiscount } from "../../redux/apiCalls";
-  import { useDispatch } from "react-redux";
+import { addDiscount } from "../../redux/apiCalls";
+import { useDispatch } from "react-redux";
 
 
 export default function NewDiscount(){
-    const [inputs, setInputs] = useState({});
-    const [file] = useState(null);
-    const [cat, setCat] = useState([]);
+    const [discounts, setInputs] = useState({});
     const dispatch = useDispatch();
 
     const handleChange = (e) => {
         setInputs((prev) => {
-        return { ...prev, [e.target.name]: e.target.value };
+          return { ...prev, [e.target.name]: e.target.value };
         });
     };
 
-    const handleCat = (e) => {
-        setCat(e.target.value.split(","));
-    };
-
     const handleClick = (e) => {
-    e.preventDefault();
-    const fileName = new Date().getTime() + file.name;
-    const storage = getStorage(app);
-    const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Observe state change events such as progress, pause, and resume
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-            default:
-          }
-        },
-        (error) => {
-          // Handle unsuccessful uploads
-        },
-        () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            const discount = { ...inputs, img: downloadURL, categories: cat };
-            addDiscount(discount, dispatch);
-          });
-        }
-      );
+      e.preventDefault();
+      addDiscount(discounts, dispatch); 
     };
 
-    
-    
     return(
         <div className="newDiscount">
             <h1 className="addDiscountTitle">Create New Discount</h1>
@@ -93,7 +40,7 @@ export default function NewDiscount(){
 
                 <div className="addDiscountItem">
                     <label>Condition</label>
-                    <input type="text" placeholder="Discount Condition" onChange={handleCat} />
+                    <input name="condition" type="number" placeholder="Discount Condition" onChange={handleChange} />
                 </div>
 
                 <button onClick={handleClick} className="addDiscountButton"> Create Discount Code </button>
