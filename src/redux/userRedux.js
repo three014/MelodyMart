c71c03200589c77f26c5feff3fe07b368fb9a82f
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-07-21 01:21:17 -0500

    Update userRedux.js
    Added User Slices for get and delete

--- src/redux/userRedux.js
+++ src/redux/userRedux.js
@@ -23,8 +23,47 @@ const userSlice = createSlice({
     logout: (state) => {
       state.currentUser = null;
     },
+    getUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    getUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products = action.payload;
+    },
+    getUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+    //DELETE
+    deleteUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    deleteUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.users.splice(
+        state.users.findIndex((item) => item._id === action.payload),
+        1
+      );
+    },
+    deleteUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export const {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+  getUserStart,
+  getUserSuccess,
+  getUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+} = userSlice.actions;
+
 export default userSlice.reducer;
