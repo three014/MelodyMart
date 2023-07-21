d7f494e1e3a56e0b994ab4c6c52d9b2335568a6b
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-07-20 19:15:27 -0500

    Update requestMethods.js
    Updated TOKEN so it won't break

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 
-// Token parse works
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+// Token parse works | Update, single parse does not, will get into inf loop with no user
+//const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 //const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWI4ZjkzMmI5MDU4ODg0NjRlMmZkYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4OTc5NDY1NCwiZXhwIjoxNjkwMDUzODU0fQ.iUhzHVuW4DgNq2mBWgAbo0zCwBtXaAmta8QUbonaEjA";
-/*
+
+// This works
 const TOKEN = () => {
     if (
       JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
@@ -16,7 +17,7 @@ const TOKEN = () => {
         .currentUser.accessToken;
     } else { return "" }
 };
-*/
+
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
