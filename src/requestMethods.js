e304a4e3f0a91c360c2f32785c7186e3173e3a69
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-07-20 21:03:35 -0500

    Update requestMethods.js
    updated token, product is broken and get is returning a 200 OK so not sure what went wrong

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -5,9 +5,10 @@ const BASE_URL = "http://localhost:5000/api/";
 // Token parse works | Update, single parse does not, will get into inf loop with no user
 //const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
-//const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWI4ZjkzMmI5MDU4ODg0NjRlMmZkYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4OTc5NDY1NCwiZXhwIjoxNjkwMDUzODU0fQ.iUhzHVuW4DgNq2mBWgAbo0zCwBtXaAmta8QUbonaEjA";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWI4ZjkzMmI5MDU4ODg0NjRlMmZkYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4OTkwNDMzMCwiZXhwIjoxNjkwMTYzNTMwfQ.Us8uPDmsqOAvjl7RFwc1LMKAzZ_1iWvkZtsbP6MgH8Q";
 
-// This works
+// This may work ?
+/*
 const TOKEN = () => {
     if (
       JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
@@ -17,7 +18,7 @@ const TOKEN = () => {
         .currentUser.accessToken;
     } else { return "" }
 };
-
+*/
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
